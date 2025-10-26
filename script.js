750cc762b7fd6da089a11943ce61cc073c73bbd5
Shermuhammad.rog <mproleo.proweb@gmai.com>
2025-10-26 14:37:28 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let count = +prompt("Nechta misol ishlamoqchisiz?");
+
+let signs = ['+', '-', '*', '/'];
+
+function RandomNum() {
+    return Math.round(Math.random() * 10);
+}
+
+for (let i = 1; i <= count; i++) {
+    let num1 = RandomNum();
+    let num2 = RandomNum();
+
+    let sign = signs[Math.round(Math.random() * signs.length)];
+
+    if (sign === '/' && num2 === 0) {
+        num2 = 1;
+    }
+
+    let correct;
+    if (sign === '+') correct = num1 + num2;
+    else if (sign === '-') correct = num1 - num2;
+    else if (sign === '*') correct = num1 * num2;
+    else correct = num1 / num2;
+
+    let userAnswer = +prompt(`${num1} ${sign} ${num2} = ?`);
+
+    if (userAnswer === correct) {
+        console.log(`Sizning javob to'g'ri: ${correct}`);
+    } else {
+        console.log(`Sizning javob noto'g'ri: ${userAnswer}. To'g'ri javob: ${correct}`);
+    }
+}
